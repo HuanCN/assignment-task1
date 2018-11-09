106e17cc32ebf05f6dec95935196b20b0edf2b29
HuanCN <timy0930@gmail.com>
2018-11-09 11:34:32 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,3 +2,11 @@
 def add (a,b):
   return a+b
 assert add(1,3) == 4
+
+numbers = [1,2,3,5]
+def sumOfList(someList):
+    result = 0
+    for index in someList:
+        result = someList.index(index)+result
+    return result
+assert sumOfList(numbers) === 11
